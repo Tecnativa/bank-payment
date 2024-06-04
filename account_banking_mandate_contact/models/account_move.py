addc0475ceb0775c76a0f3aedbd5caac9c829b5b
Carlos Dauden <carlos.dauden@tecnativa.com>
2024-06-04 18:17:35 +0200

    [IMP] account_banking_mandate_contact: Show contact mandate account bank in invoice report TT49436

--- account_banking_mandate_contact/models/account_move.py
+++ account_banking_mandate_contact/models/account_move.py
@@ -7,6 +7,17 @@ from odoo import api, models
 class AccountMove(models.Model):
     _inherit = "account.move"
 
+    def partner_banks_to_show(self):
+        self.ensure_one()
+        p_shipping_bank_id = self.partner_shipping_id.valid_mandate_id.partner_bank_id
+        if (
+            p_shipping_bank_id
+            and not self.partner_bank_id
+            and self.payment_mode_id.payment_method_id.code == "sepa_direct_debit"
+        ):
+            return p_shipping_bank_id
+        return super().partner_banks_to_show()
+
     def set_mandate(self):
         res = super().set_mandate()
         if (
