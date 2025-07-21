108372a094fe83c05b80ee2014d8f8aa8f5c1f75
enricostano <enricostn@gmail.com>
2025-07-21 12:04:46 +0200

    [FIX] account_payment_order_grouped_output: use computed outstanding account
    This allows to use general type journals as transfer journal, otherwise
    the order's invoices will not be reconciled and will remain in
    in_payment status.
    
    The field is defined here: https://github.com/OCA/OCB/blob/8573e598bea26f18cb062be589ddf4ed1e2b8990/addons/account/models/account_payment.py#L95
    And computed, for both inbound and outbound, here: https://github.com/OCA/OCB/blob/8573e598bea26f18cb062be589ddf4ed1e2b8990/addons/account/models/account_payment.py#L495
    
    This is the same field used while creating the move associated with the
    payment: https://github.com/OCA/OCB/blob/8573e598bea26f18cb062be589ddf4ed1e2b8990/addons/account/models/account_payment.py#L325

--- account_payment_order_grouped_output/models/account_payment_order.py
+++ account_payment_order_grouped_output/models/account_payment_order.py
@@ -78,13 +78,8 @@ class AccountPaymentOrder(models.Model):
     def reconcile_grouped_payments(self, move, payments):
         lines_to_rec = move.line_ids[:-1]
         for payment in payments:
-            journal = payment.journal_id
             lines_to_rec += payment.move_id.line_ids.filtered(
-                lambda x: x.account_id
-                in (
-                    journal._get_journal_inbound_outstanding_payment_accounts()
-                    + journal._get_journal_outbound_outstanding_payment_accounts()
-                )
+                lambda x: x.account_id == payment.outstanding_account_id
             )
         lines_to_rec.reconcile()
 
